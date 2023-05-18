6ba475868e0de57fa15d88e0561ca26c6a6704a9
Pavel Ordenov <pavelordenov@yandex.ru>
2023-05-18 21:36:50 +0200

    Added project with IfElse Syntax

---
+++ Example005_IfElseSyntax/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите Ваше имя, ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+    {
+        Console.WriteLine("Сильно тебя приветствую, Маша");
+    }
+else
+    {
+        Console.WriteLine("Мы тебе не рады, " + username);
+    }
