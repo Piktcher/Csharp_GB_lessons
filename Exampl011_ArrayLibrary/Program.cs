8d1ba9d798ad69716271f36ab6389e9602464f15
Pavel Ordenov <pavelordenov@yandex.ru>
2023-05-21 13:01:39 +0200

    Added tasks 10 and 11

---
+++ Exampl011_ArrayLibrary/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)    // Void метод ничего не возвращает, внутри него не требуется оператор return
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection2)
+{
+    int count = collection2.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(collection2[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
