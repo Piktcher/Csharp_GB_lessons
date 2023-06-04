cf9d97cc48b68f4207fac3b8beae93ed4b1219a9
Pavel Ordenov <pavelordenov@yandex.ru>
2023-06-04 13:37:41 +0200

    Added examples 15 and 16

---
+++ Example015_TwoDimArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// СОздание, наполнение и вывод двумерных массивов
+
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int col = 0; col < matr.GetLength(1); col++)
+        {
+            matr[rows, col] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[2, 5];
+FillArray(matrix);
+PrintArray(matrix);
